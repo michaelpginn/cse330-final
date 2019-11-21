9a0d89bca5c9bdd231d9fcedd2fbd2d50ce4c821
Michael Ginn <milespergallon271@gmail.com>
2019-11-21 02:42:43 -0600

    add basic structure (not working tho)

--- hfs/src/App.js
+++ hfs/src/App.js
@@ -1,11 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
+import * as SWRTC from '@andyet/simplewebrtc';
+
+const API_KEY = "9efd1b765b3af86fbdf1cd42";
+const CONFIG_URL = `https://api.simplewebrtc.com/config/guest/${API_KEY}`;
+
+const store = SWRTC.createStore();
 
 function App() {
   return (
     <div className="App">
-      
+      <Provider store={store}>
+        <SWRTC.Provider configUrl={CONFIG_URL}>
+          <SWRTC.Connecting>
+            <h1>Connecting...</h1>
+          </SWRTC.Connecting>
+          <SWRTC.Connected>
+            <h1>Connected!</h1>
+            {/* Request user's info */}
+            <SWRTC.RequestUserMedia audio video auto />
+
+            {/* Enable playing remote audio. */}
+            <SWRTC.RemoteAudioPlayer />
+          
+            {/* Connect to a room with a name and optional password */}
+            <SWRTC.Room name="test" >
+              {props => {
+                return <div>oop</div>
+              }}
+            </SWRTC.Room>
+          </SWRTC.Connected>
+
+        </SWRTC.Provider>
+      </Provider>
     </div>
   );
 }
