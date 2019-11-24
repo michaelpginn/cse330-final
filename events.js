64d01216037af92a72fb61ad4d4d089daaa514c8
Michael Ginn <milespergallon271@gmail.com>
2019-11-24 13:55:41 -0600

    create events.js to hold event names

---
+++ events.js
@@ -0,0 +1,10 @@
+exports.Events = {
+    SET_USERNAME: "set_username",
+    FIND_ROOM: "find_room",
+    SEND_MESSAGE: "send_message",
+    SEND_IMAGE_MESSAGE: "send_image_message",
+    DISCONNECT: "disconnect",
+    ROOM_CHANGED: "room_changed",
+    NEW_MESSAGE: "new_message",
+    ROOM_MEMBERS_CHANGED: "room_members_changed"
+}
