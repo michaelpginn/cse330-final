6d2aa7b2db84ac9301cb11380bc27f06e745f5bf
Michael Ginn <milespergallon271@gmail.com>
2019-11-26 20:17:00 -0600

    add listeners for all methods

--- hfs/src/Components/FriendSimulatorApp.js
+++ hfs/src/Components/FriendSimulatorApp.js
@@ -15,22 +15,50 @@ export default class FriendSimulatorApp extends React.Component{
         this.state = {
             connected: false,
             currentUsername: null,
+            currentRoom: null, //{roomId, otherUser: {username, rating}, messages: []}
             error: null
         }
     }
     componentDidMount = async () => {
         try {
             this.socket = openSocket(INSTANCE_URL);
-            this.socket.on('connect', () => { this.setState({connected: true}) })
+            this.socket.on('connect', () => { this.setState({ connected: true }) });
+            this.socket.on('room_changed', this.room_changed);
+            this.socket.on('new_message', this.new_message);
         } catch (error) {
             console.log(error);
         }
-        
     };
 
+    // listener funcs
+    room_changed = (roomId, otherUser) => {
+        console.log(roomId);
+        if (!roomId) {
+            // we are not in a room now
+            this.setState({ currentRoom: null });
+        } else {
+            this.setState({ currentRoom: { roomId, otherUser, messages: [] } });
+        }
+    }
+
+    new_message = (messageText, senderUsername) => {
+        const { currentRoom } = this.state;
+        const newMessage = { senderUsername, messageText }
+        const newRoom = { ...currentRoom, messages: [...currentRoom.messages, newMessage] }
+        console.log(newRoom);
+        this.setState({ currentRoom: newRoom });
+    };
+
+    // emitter funcs
     set_username = username => {
         this.socket.emit("set_username", username, (error) => { this.usernameErrorFunc(error, username) });
     };
+
+    send_message = (messageText) => {
+        this.socket.emit("send_message", messageText, this.errorFunc);
+    };
+
+
 
     usernameErrorFunc = (error, username) => {
         if (error) {
@@ -47,35 +75,32 @@ export default class FriendSimulatorApp extends React.Component{
         setTimeout(() => { this.setState({ error: null }) }, 4000)
     }
 
-    send_message = (messageText) => {
-        this.socket.emit("send_message", messageText, this.errorFunc);
-    }
+    
 
     render() {
-        const { connected, currentUsername, error } = this.state;
-        let currentRoom = { users: [currentUsername, null], messages: [] };
+        const { connected, currentUsername, error, currentRoom } = this.state;
         return (
             <div style={{ display: "flex", height: "100vh", flexDirection: "column", }}>
                 <Header />
                 <div style={{ display: "flex", alignItems: "stretch", flex: 1 }}>
                     {currentUsername ? (
                         <React.Fragment>
-                            <div>
-                            <h1>Hey, {currentUsername}!</h1>
-                            {/* Connect to a room with a name */}
                             {connected ? (
-
-                                <SWRTC.Room name="test" >
-                                    {() => {
-                                        return <VideoScreen />
-                                    }}
-                                </SWRTC.Room>
-                            )
+                                <div>
+                                    <div>
+                                        <h1>Hey, {currentUsername}!</h1>
+                                        {/* Connect to a room with a name */}
+                         
+                                        <SWRTC.Room name={currentRoom ? currentRoom.roomId : currentUsername} >
+                                            {() => {
+                                                return <VideoScreen />
+                                            }}
+                                        </SWRTC.Room>
+                                    </div>
+                                    <ChatWrapper currentRoom={currentRoom} send_message={this.send_message} />
+                                </div>)
                                 :
-                                <div>Could not connect to socket, server may not be running.</div>
-                                }
-                            </div>
-                            <ChatWrapper currentRoom={currentRoom} send_message={this.send_message} />
+                                (<div>Could not connect to socket, server may not be running.</div>)}
                         </React.Fragment>
                     )
                         : (
