ae9e384271c88ba8f040394f4b0b70313485e7e6
kathleenlund <kathleen.lund@wustl.edu>
2019-12-02 19:37:56 -0600

    filters

--- hfs/src/Components/rating.js
+++ hfs/src/Components/rating.js
@@ -25,19 +25,19 @@ export default class extends React.Component {
                 <div className="chat-input" >
                     {sent ? (
                     <div>
-                        {currentRoom && (<text style={{ fontSize: '3vh' }}>Rating Sent! </text>)}
-                        {currentRoom && (<text style={{ fontSize: '3vh' }}>{currentRoom.otherUser.username}'s Current Rating: {currentRoom.otherUser.rating}</text>)}
+                        {currentRoom && (<text style={{ fontSize: '2vh' }}>Rating Sent! </text>)}
+                        {currentRoom && (<text style={{ fontSize: '2vh' }}>{currentRoom.otherUser.username}'s Current Rating: {currentRoom.otherUser.rating}</text>)}
                     </div>
                     ): (
                     <div>
-                        {currentRoom && (<text style={{ fontSize: '3vh' }}>Rate {currentRoom.otherUser.username}:</text>)}
-                        <input type="radio" id="one" value="1" onClick={this.changeRating} />1★
-                        <input type="radio" id="two" value="2" onClick={this.changeRating} />2★
-                        <input type="radio" id="three" value="3" onClick={this.changeRating} />3★
-                        <input type="radio" id="four" value="4" onClick={this.changeRating} />4★
-                        <input type="radio" id="five" value="5" onClick={this.changeRating} />5★
+                        {currentRoom && (<text style={{ fontSize: '2vh' }}>Rate {currentRoom.otherUser.username}:</text>)}
+                        <input type="radio" name="rating" id="one" value="1" onClick={this.changeRating} />1★
+                        <input type="radio" name="rating" id="two" value="2" onClick={this.changeRating} />2★
+                        <input type="radio" name="rating" id="three" value="3" onClick={this.changeRating} />3★
+                        <input type="radio" name="rating" id="four" value="4" onClick={this.changeRating} />4★
+                        <input type="radio" name="rating" id="five" value="5" onClick={this.changeRating} />5★
                         <button className="big-button" onClick={this.rateUser}>Send Rating!</button>
-                        {currentRoom && (<text style={{ fontSize: '3vh' }}>{currentRoom.otherUser.username}'s Current Rating: {currentRoom.otherUser.rating}</text>)}
+                        {currentRoom && (<text style={{ fontSize: '2vh' }}>{currentRoom.otherUser.username}'s Current Rating: {currentRoom.otherUser.rating}</text>)}
                     </div>
                         )}
                 
--- hfs/src/Components/videoscreen.js
+++ hfs/src/Components/videoscreen.js
@@ -1,35 +1,211 @@
 import React from "react";
 import { LocalMediaList, RemoteMediaList } from '@andyet/simplewebrtc';
 import MediaPreview from './mediapreview';
+import Filter from './filter';
 
-export default function VideoScreen() {
+export default class VideoScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { filter: null };
+    }
 
-    return (
-        <div style={{width: "62vh"}}>
-            <LocalMediaList
-                screen={false}
-                render={({ media }) => {
-                    //console.log(media);
-                    const audioStreams = media.filter(m => m.kind === 'audio');
-                    const videoStreams = media.filter(m => m.kind === 'video');
-                    const latestAudio = audioStreams[audioStreams.length - 1];
-                    const latestVideo = videoStreams[videoStreams.length - 1];
-                    return <MediaPreview video={latestVideo} audio={latestAudio} />;
-                }}
-            />
-            <div style={{ height: 1 }} />
-            <RemoteMediaList
-                audio={true}
-                video={true}
-                render={({ media }) => {
-                    //console.log(media);
-                    const audioStreams = media.filter(m => m.kind === 'audio');
-                    const videoStreams = media.filter(m => m.kind === 'video');
-                    const latestAudio = audioStreams[audioStreams.length - 1];
-                    const latestVideo = videoStreams[videoStreams.length - 1];
-                    return <MediaPreview video={latestVideo} audio={latestAudio} />;
-                }}
-            />
-        </div>
-    );
+    set_filter = filter => {
+        this.setState({ filter: filter });
+    };
+
+    render() {
+        const { filter } = this.state;
+        return (
+            <div>
+                {filter === "red" && 
+                    <div className="red-filter" >
+                        {/* <div style={{ width: "62vh" }}> */}
+                        <LocalMediaList
+                            screen={false}
+                            render={({ media }) => {
+                                //console.log(media);
+                                const audioStreams = media.filter(m => m.kind === 'audio');
+                                const videoStreams = media.filter(m => m.kind === 'video');
+                                const latestAudio = audioStreams[audioStreams.length - 1];
+                                const latestVideo = videoStreams[videoStreams.length - 1];
+                                return <MediaPreview video={latestVideo} audio={latestAudio} />;
+                            }}
+                        />
+
+                        <div style={{ height: 1 }} />
+                        <RemoteMediaList
+                            audio={true}
+                            video={true}
+                            render={({ media }) => {
+                                //console.log(media);
+                                const audioStreams = media.filter(m => m.kind === 'audio');
+                                const videoStreams = media.filter(m => m.kind === 'video');
+                                const latestAudio = audioStreams[audioStreams.length - 1];
+                                const latestVideo = videoStreams[videoStreams.length - 1];
+                                return <MediaPreview video={latestVideo} audio={latestAudio} />;
+                            }}
+                        />
+                        {/* </div> */}
+                    </div>
+                }
+                {filter === "green" &&
+                    <div className="green-filter" >
+                        {/* <div style={{ width: "62vh" }}> */}
+                        <LocalMediaList
+                            screen={false}
+                            render={({ media }) => {
+                                //console.log(media);
+                                const audioStreams = media.filter(m => m.kind === 'audio');
+                                const videoStreams = media.filter(m => m.kind === 'video');
+                                const latestAudio = audioStreams[audioStreams.length - 1];
+                                const latestVideo = videoStreams[videoStreams.length - 1];
+                                return <MediaPreview video={latestVideo} audio={latestAudio} />;
+                            }}
+                        />
+
+                        <div style={{ height: 1 }} />
+                        <RemoteMediaList
+                            audio={true}
+                            video={true}
+                            render={({ media }) => {
+                                //console.log(media);
+                                const audioStreams = media.filter(m => m.kind === 'audio');
+                                const videoStreams = media.filter(m => m.kind === 'video');
+                                const latestAudio = audioStreams[audioStreams.length - 1];
+                                const latestVideo = videoStreams[videoStreams.length - 1];
+                                return <MediaPreview video={latestVideo} audio={latestAudio} />;
+                            }}
+                        />
+                        {/* </div> */}
+                    </div>
+                }
+                {filter === "blue" &&
+                    <div className="blue-filter" >
+                        {/* <div style={{ width: "62vh" }}> */}
+                        <LocalMediaList
+                            screen={false}
+                            render={({ media }) => {
+                                //console.log(media);
+                                const audioStreams = media.filter(m => m.kind === 'audio');
+                                const videoStreams = media.filter(m => m.kind === 'video');
+                                const latestAudio = audioStreams[audioStreams.length - 1];
+                                const latestVideo = videoStreams[videoStreams.length - 1];
+                                return <MediaPreview video={latestVideo} audio={latestAudio} />;
+                            }}
+                        />
+
+                        <div style={{ height: 1 }} />
+                        <RemoteMediaList
+                            audio={true}
+                            video={true}
+                            render={({ media }) => {
+                                //console.log(media);
+                                const audioStreams = media.filter(m => m.kind === 'audio');
+                                const videoStreams = media.filter(m => m.kind === 'video');
+                                const latestAudio = audioStreams[audioStreams.length - 1];
+                                const latestVideo = videoStreams[videoStreams.length - 1];
+                                return <MediaPreview video={latestVideo} audio={latestAudio} />;
+                            }}
+                        />
+                        {/* </div> */}
+                    </div>
+                }
+                {filter === "warm" &&
+                    <div className="warm-filter" >
+                        {/* <div style={{ width: "62vh" }}> */}
+                        <LocalMediaList
+                            screen={false}
+                            render={({ media }) => {
+                                //console.log(media);
+                                const audioStreams = media.filter(m => m.kind === 'audio');
+                                const videoStreams = media.filter(m => m.kind === 'video');
+                                const latestAudio = audioStreams[audioStreams.length - 1];
+                                const latestVideo = videoStreams[videoStreams.length - 1];
+                                return <MediaPreview video={latestVideo} audio={latestAudio} />;
+                            }}
+                        />
+
+                        <div style={{ height: 1 }} />
+                        <RemoteMediaList
+                            audio={true}
+                            video={true}
+                            render={({ media }) => {
+                                //console.log(media);
+                                const audioStreams = media.filter(m => m.kind === 'audio');
+                                const videoStreams = media.filter(m => m.kind === 'video');
+                                const latestAudio = audioStreams[audioStreams.length - 1];
+                                const latestVideo = videoStreams[videoStreams.length - 1];
+                                return <MediaPreview video={latestVideo} audio={latestAudio} />;
+                            }}
+                        />
+                        {/* </div> */}
+                    </div>
+                }
+                {filter === "cool" &&
+                    <div className="cool-filter" >
+                        {/* <div style={{ width: "62vh" }}> */}
+                        <LocalMediaList
+                            screen={false}
+                            render={({ media }) => {
+                                //console.log(media);
+                                const audioStreams = media.filter(m => m.kind === 'audio');
+                                const videoStreams = media.filter(m => m.kind === 'video');
+                                const latestAudio = audioStreams[audioStreams.length - 1];
+                                const latestVideo = videoStreams[videoStreams.length - 1];
+                                return <MediaPreview video={latestVideo} audio={latestAudio} />;
+                            }}
+                        />
+
+                        <div style={{ height: 1 }} />
+                        <RemoteMediaList
+                            audio={true}
+                            video={true}
+                            render={({ media }) => {
+                                //console.log(media);
+                                const audioStreams = media.filter(m => m.kind === 'audio');
+                                const videoStreams = media.filter(m => m.kind === 'video');
+                                const latestAudio = audioStreams[audioStreams.length - 1];
+                                const latestVideo = videoStreams[videoStreams.length - 1];
+                                return <MediaPreview video={latestVideo} audio={latestAudio} />;
+                            }}
+                        />
+                        {/* </div> */}
+                    </div>
+                }
+                {filter === null &&
+                    <div style={{ width: "62vh" }}>
+                        {/* <div style={{ width: "62vh" }}> */}
+                        <LocalMediaList
+                            screen={false}
+                            render={({ media }) => {
+                                //console.log(media);
+                                const audioStreams = media.filter(m => m.kind === 'audio');
+                                const videoStreams = media.filter(m => m.kind === 'video');
+                                const latestAudio = audioStreams[audioStreams.length - 1];
+                                const latestVideo = videoStreams[videoStreams.length - 1];
+                                return <MediaPreview video={latestVideo} audio={latestAudio} />;
+                            }}
+                        />
+
+                        <div style={{ height: 1 }} />
+                        <RemoteMediaList
+                            audio={true}
+                            video={true}
+                            render={({ media }) => {
+                                //console.log(media);
+                                const audioStreams = media.filter(m => m.kind === 'audio');
+                                const videoStreams = media.filter(m => m.kind === 'video');
+                                const latestAudio = audioStreams[audioStreams.length - 1];
+                                const latestVideo = videoStreams[videoStreams.length - 1];
+                                return <MediaPreview video={latestVideo} audio={latestAudio} />;
+                            }}
+                        />
+                        {/* </div> */}
+                    </div>
+                }
+
+                <Filter set_filter={this.set_filter} />
+            </div>
+        );
+    } 
 }
