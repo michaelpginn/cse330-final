b7d5c62fff9d246aa4d0145ad49c8ae9f54dfd4d
kathleenlund <kathleen.lund@wustl.edu>
2019-12-04 00:31:47 -0600

    Send gif message frontend methods

--- hfs/src/Components/chatwrapper.js
+++ hfs/src/Components/chatwrapper.js
@@ -3,7 +3,10 @@ import React from "react";
 export default class ChatWrapper extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { newMessage: "" };
+        this.state = {
+            newMessage: "",
+            gifCode: null
+        };
     }
 
     changeMessage = event => {
@@ -16,6 +19,13 @@ export default class ChatWrapper extends React.Component {
         send_message(newMessage);
         this.setState({ newMessage: "" });
     };
+
+    sendGifMessage = () => {
+        const { send_gif_message } = this.props;
+        const { gifCode } = this.state;
+        send_gif_message(gifCode);
+        this.setState({ gifCode: null });
+    }
 
     handleKeyPress = (event) => {
         const { exit_room } = this.props;
